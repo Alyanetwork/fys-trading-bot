d993010d44e00c89ed40def69962a143010b2584
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-13 02:25:12 +0300

    Create wallet_manager.py

---
+++ app/wallet_manager.py
@@ -0,0 +1,12 @@
+class WalletManager:
+    def __init__(self):
+        self.assets = {}
+
+    def update_asset(self, symbol, amount):
+        if symbol in self.assets:
+            self.assets[symbol] += amount
+        else:
+            self.assets[symbol] = amount
+
+    def get_balance(self):
+        return self.assets
